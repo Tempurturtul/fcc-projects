00c263798bfeab0f4a6c9f4c881af4c128dd6fee
Tempurturtul <tempurturtul@gmail.com>
2017-01-08 15:37:59 -0500

    Updates

---
+++ tasks/deploy.js
@@ -0,0 +1,17 @@
+const ghpages = require('gh-pages');
+const path = require('path');
+const options = {
+  src: [
+    '*',
+    '*/**',
+    '!node_modules',
+  ],
+  branch: 'heroku-url-shortener-microservice',
+};
+
+ghpages.publish(path.join(__dirname, '..'), options, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Successfully deployed.');
+});
