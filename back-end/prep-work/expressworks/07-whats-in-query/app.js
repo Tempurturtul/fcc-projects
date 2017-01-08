63c7d89d69b72b3803c92adca1d89b49574d9291
Tempurturtul <tempurturtul@gmail.com>
2017-01-08 11:44:38 -0500

    Remove all except distribution files

--- back-end/prep-work/expressworks/07-whats-in-query/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const port = process.argv[2];
-const app = express();
-
-app.get('/search', (request, response) => {
-  response.send(request.query);
-});
-
-app.listen(port);
