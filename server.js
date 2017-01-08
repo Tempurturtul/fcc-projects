4200f82769837358d435ac5ecb809121f4424bb5
Tempurturtul <tempurturtul@gmail.com>
2017-01-08 09:06:52 -0500

    Update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const useragent = require('express-useragent');
+const getip = require('ipware')().get_ip;
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -7,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(useragent.express());
 
 app.get('*', (request, response) => {
-  const ipaddress = request.ip;
+  const ipaddress = getip(request).clientIp;
   const language = request.acceptsLanguages()[0];
   const os = request.useragent.os;
 
